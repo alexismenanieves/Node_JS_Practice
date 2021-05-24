ad260168519b9aa0f1cf8a5c4f3fd279970a9c51
alexismenanieves <alexismenanieves@hotmail.com>
2021-05-24 18:34:25 -0500

    An improved edition

--- Simple_Server.js
+++ Simple_Server.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 app.get('/', (req, res) => {
   res.send('Hi!')
-})
+});
 
-app.listen(3000, () => console.log('Server ready'))
+app.listen(3000, () => console.log('Server ready'));
 
 process.on('SIGTERM', () =>{
   server.close(() => {
-    console.log('Process terminated')
+    console.log('Process terminated');
   })
-})
+});

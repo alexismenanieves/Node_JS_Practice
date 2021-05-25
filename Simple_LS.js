a2663a8acd9ff9957048166db79e203975ef0ad2
alexismenanieves <alexismenanieves@hotmail.com>
2021-05-24 21:46:08 -0500

    Added some additional comments

--- Simple_LS.js
+++ Simple_LS.js
@@ -1,7 +1,9 @@
+// We use const to avoid changing the received module
 const fs = require('fs');
 const util = require('util');
 const fs_readdir = util.promisify(fs.readdir);
 
+// Now an async anonymous function to list all files 
 (async () => {
   const files = await fs_readdir('.');
   for(let fn of files) {

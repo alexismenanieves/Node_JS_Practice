0193862911c9a25370e7d71c52683a41cdb213d8
alexismenanieves <alexismenanieves@hotmail.com>
2021-05-24 21:43:38 -0500

    Added comments

--- Simple_Server.js
+++ Simple_Server.js
@@ -1,3 +1,4 @@
+// We use const to avoid changing the received module
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
@@ -5,7 +6,7 @@ app.get('/', (req, res) => {
 });
 
 app.listen(3000, () => console.log('Server ready'));
-
+// In case we want to end the process
 process.on('SIGTERM', () =>{
   server.close(() => {
     console.log('Process terminated');
